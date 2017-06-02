7df007d07966e770212a8c551a00ea3e7804f47d
Brewbaker, Chad <Brewbaker.Chad@principal.com>
2017-06-02 09:42:01 -0500

    Adding curry/uncurry

---
+++ algebraic-refactor.hs
@@ -0,0 +1,9 @@
+-- | 'curry' converts an uncurried function to a curried function.
+curry                   :: ((a, b) -> c) -> a -> b -> c
+curry f x y             =  f (x, y)
+
+-- | 'uncurry' converts a curried function to a function on pairs.
+uncurry                 :: (a -> b -> c) -> ((a, b) -> c)
+uncurry f p             =  f (fst p) (snd p)
+
+--- Reference code from http://hackage.haskell.org/package/base-4.9.1.0/docs/src/Data.Tuple.html#curry
